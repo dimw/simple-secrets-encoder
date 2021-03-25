d9f842e952aee62197ee5a9b51e910e6e0dc9d8e
dimw <dimw@gmx.de>
2021-03-25 09:07:57 +0100

    Remove usage of deprecated method

--- rsaio/rsaio.go
+++ rsaio/rsaio.go
@@ -4,16 +4,8 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"errors"
-	"fmt"
 	"io/ioutil"
 )
-
-var errEncryptedPEMBlockNotSupported = errors.New("encrypted PEM blocks are not supported")
-
-func EncryptedPEMBlockNotSupportedError() error {
-	return fmt.Errorf("%w", errEncryptedPEMBlockNotSupported)
-}
 
 func LoadPublicKey(filename string) (*rsa.PublicKey, error) {
 	data, err := ioutil.ReadFile(filename)
@@ -36,9 +28,7 @@ func LoadPrivateKey(filename string) (*rsa.PrivateKey, error) {
 
 	p, _ := pem.Decode(data)
 
-	if x509.IsEncryptedPEMBlock(p) {
-		return nil, EncryptedPEMBlockNotSupportedError()
-	}
+	// TODO use passkey if needed
 
 	key, err := x509.ParsePKCS1PrivateKey(p.Bytes)
 
